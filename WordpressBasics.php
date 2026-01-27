36e033bca8582ce8818d75500924099feb5c1d84
ammar869 <ammarhassan1131318@gmail.com>
2026-01-27 23:05:32 +0500

    Syntax of the add_action

--- WordpressBasics.php
+++ WordpressBasics.php
@@ -1,1 +1,11 @@
 <?php
+// Two type of functions in php
+// wordpress is the funcction heavy
+
+// Basic syntax 
+
+//1
+function my_theme_setup() {
+    add_theme_support('post-thumbnails');
+}
+add_action('after_setup_theme', 'my_theme_setup');
